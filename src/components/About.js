739c77e4c79d8c270c22340bc8cadde4ed543fee
Sean Zweifel <spzweifel@gmail.com>
2023-07-30 20:11:58 -0400

    sks

--- src/components/About.js
+++ src/components/About.js
@@ -9,7 +9,7 @@ const About = () => {
                     <h1>About Me</h1>
                 </div>
                 {/* get this image to start working correctly*/}
-                <img src='.\images\triskelion.png' alt='triskelion'/>
+                <img src='images/triskelion.png' alt='triskelion'/>
                 <p>My name is Sean, an aspiring fullstack developer based in Richmond, Virginia, USA. I originally graduated from Virginia Commonwealth University with a bachelors of science in forensic science with a chemistry minor. I have been working extensively over the past 4 years in gun crime intelligence in Richmond City. After a short but rewarding career in forensics, I've decided to make the transition to web development. Although, I'm new to this field, I'm excited to see where it takes me and to help improve where ever I can.</p>
             </div>
         </section>
