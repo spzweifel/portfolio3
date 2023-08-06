a8bac69323ea1ab5ed362dc6459ddb1d243e45a5
Sean Zweifel <spzweifel@gmail.com>
2023-08-06 19:36:23 -0400

    profile pic

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,7 @@ import {ReactComponent as GITHUB} from '../resources/icons/github.svg'
 import {ReactComponent as INSTAGRAM} from '../resources/icons/instagram.svg'
 import {ReactComponent as TWITTER} from '../resources/icons/twitter.svg'
 import {ReactComponent as LINKEDIN} from '../resources/icons/linkedin.svg'
+import 'bulma/css/bulma.min.css';
 
 const Footer = () => {
   const socialLinks = [
