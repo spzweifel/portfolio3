84e767ef1f8d6a945965d0df909cd531f64516e3
Sean Zweifel <spzweifel@gmail.com>
2023-08-26 15:06:21 -0400

    changed the sizes of the svg files

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {ReactComponent as GITHUB} from '../resources/icons/github.svg'
-import {ReactComponent as INSTAGRAM} from '../resources/icons/instagram.svg'
-import {ReactComponent as TWITTER} from '../resources/icons/twitter.svg'
+// import {ReactComponent as INSTAGRAM} from '../resources/icons/instagram.svg'
+// import {ReactComponent as TWITTER} from '../resources/icons/twitter.svg'
 import {ReactComponent as LINKEDIN} from '../resources/icons/linkedin.svg'
 import 'bulma/css/bulma.min.css';
 
@@ -10,14 +10,6 @@ const Footer = () => {
     {
       link: 'https://github.com/spzweifel',
       icon: <GITHUB />,
-    },
-    {
-      link: 'https://www.instagram.com/sean.zweifel/',
-      icon: <INSTAGRAM />,
-    },
-    {
-      link: 'https://twitter.com/',
-      icon: <TWITTER />,
     },
     {
       link: 'https://www.linkedin.com/in/sean-zweifel-161768144/',
